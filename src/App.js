cabc0a7dcef19d44089311c9289e7986efd4dc74
murshidazher <murshidazher@yahoo.com>
2020-04-04 22:11:05 +0530

    feat(movieForm): add movies form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/common/navbar";
+import "./App.css";
+import RegisterForm from "./components/registerForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/register" component={RegisterForm}></Route>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
